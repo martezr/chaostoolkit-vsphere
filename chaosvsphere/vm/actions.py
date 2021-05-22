3f3521545a8305d960cbcf4dc83b26f0d1152684
Martez Reed <martez.reed@gmail.com>
2021-05-22 09:17:06 -0500

    Update actions.py

--- chaosvsphere/vm/actions.py
+++ chaosvsphere/vm/actions.py
@@ -11,7 +11,7 @@ from chaosvsphere.utils import WaitForTasks
 from logzero import logger
 from pyVmomi import vim, vmodl
 
-__all__ = ["start_vm", "stop_vm"]
+__all__ = ["start_vm", "stop_vm", "connect_nic", "disconnect_nic"]
 
 
 def start_vm(vm_name: str, configuration: Configuration = None,
@@ -46,7 +46,7 @@ def stop_vm(vm_name: str, configuration: Configuration = None,
     return '{"status":"VM powered off"}'
 
 
-def disconnect_nic(vm_name: str, nic_id: int,
+def connect_nic(vm_name: str, nic_number: int,
                    configuration: Configuration = None,
                    secrets: Secrets = None):
     """
@@ -59,10 +59,73 @@ def disconnect_nic(vm_name: str, nic_id: int,
     vmList = objView.view
     objView.Destroy()
 
-    # Find the vm and power it on
-    tasks = [vm.PowerOffVM_Task() for vm in vmList if vm.name == vm_name]
-    WaitForTasks(tasks, client)
-    return '{"status":"VM powered off"}'
+    for vm in vmList:
+        if vm.name == vm_name:
+            vm_obj = vm
+
+    nic_prefix_label = 'Network adapter '
+    nic_label = nic_prefix_label + str(nic_number)
+    virtual_nic_device = None
+    for dev in vm_obj.config.hardware.device:
+        if isinstance(dev, vim.vm.device.VirtualEthernetCard) \
+                and dev.deviceInfo.label == nic_label:
+            virtual_nic_device = dev
+
+    virtual_nic_spec = vim.vm.device.VirtualDeviceSpec()
+    virtual_nic_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.edit
+    virtual_nic_spec.device = virtual_nic_device
+
+    connectable = vim.vm.device.VirtualDevice.ConnectInfo()
+    connectable.connected = True
+    virtual_nic_spec.device.connectable = connectable
+    dev_changes = []
+    dev_changes.append(virtual_nic_spec)
+    spec = vim.vm.ConfigSpec()
+    spec.deviceChange = dev_changes
+    task = vm_obj.ReconfigVM_Task(spec=spec)
+    WaitForTasks([task], client)
+    return '{"status":"Nic connected"}'
+
+
+def disconnect_nic(vm_name: str, nic_number: int,
+                   configuration: Configuration = None,
+                   secrets: Secrets = None):
+    """
+    """
+    client = vsphere_client(configuration, secrets)
+    content = client.content
+    objView = content.viewManager.CreateContainerView(content.rootFolder,
+                                                      [vim.VirtualMachine],
+                                                      True)
+    vmList = objView.view
+    objView.Destroy()
+
+    for vm in vmList:
+        if vm.name == vm_name:
+            vm_obj = vm
+
+    nic_prefix_label = 'Network adapter '
+    nic_label = nic_prefix_label + str(nic_number)
+    virtual_nic_device = None
+    for dev in vm_obj.config.hardware.device:
+        if isinstance(dev, vim.vm.device.VirtualEthernetCard) \
+                and dev.deviceInfo.label == nic_label:
+            virtual_nic_device = dev
+
+    virtual_nic_spec = vim.vm.device.VirtualDeviceSpec()
+    virtual_nic_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.edit
+    virtual_nic_spec.device = virtual_nic_device
+
+    connectable = vim.vm.device.VirtualDevice.ConnectInfo()
+    connectable.connected = False
+    virtual_nic_spec.device.connectable = connectable
+    dev_changes = []
+    dev_changes.append(virtual_nic_spec)
+    spec = vim.vm.ConfigSpec()
+    spec.deviceChange = dev_changes
+    task = vm_obj.ReconfigVM_Task(spec=spec)
+    WaitForTasks([task], client)
+    return '{"status":"Nic disconnected"}'
 
 
 def destroy_vm(vm_name: str, configuration: Configuration = None,
